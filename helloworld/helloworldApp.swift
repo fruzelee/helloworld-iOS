28a856f6ccc0601fe9a77ed39d780629fd78593f
fruzelee <fazlerabbicse@gmail.com>
2023-07-19 02:53:25 -0700

    Initial Commit

---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by FR on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
